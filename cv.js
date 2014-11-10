90eff05cb638d351701d49da969a0cae79afee2a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-09 23:09:27 -0500

    Handling resize and scroll

---
+++ cv.js
@@ -0,0 +1,45 @@
+function main() {
+    "use strict";
+
+    var
+        _width,
+        _height,
+        _scrollY = 0;
+
+    function updateDebugInfo() {
+        var debugPanel = document.getElementById("debug");
+        debugPanel.innerHTML = [
+            "<table>",
+            "<tr><td>Width:</td><td>", _width, "</td></tr>",
+            "<tr><td>Height:</td><td>", _height, "</td></tr>",
+            "<tr><td>Scroll Y:</td><td>", _scrollY, "</td></tr>",
+            "</table>"
+        ].join("");
+        debugPanel.style.top = _scrollY + "px";
+    }
+
+    function _onScroll(event) {
+        event = event || window.event;
+        _scrollY = window.scrollY;
+        updateDebugInfo();
+    }
+
+    function _onResize(event) {
+        event = event || window.event;
+        _width = window.innerWidth;
+        _height = window.innerHeight;
+        updateDebugInfo();
+    }
+
+    // Install handlers
+    window.onscroll = _onScroll;
+    window.onresize = _onResize;
+
+    _onResize();
+    _onScroll();
+}
+
+window.onload=function () {
+    "use strict";
+    gpf.loaded(main);
+};
