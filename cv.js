e9f00739e772daaa56e7ce2c1efc4766cd154ea3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-12-13 00:36:46 -0500

    Integrated responsive framework

--- cv.js
+++ cv.js
@@ -2,48 +2,41 @@ function main() {
     "use strict";
 
     var
+        _debug = document.getElementById("debug"),
         _width,
         _height,
         _scrollY = 0;
 
     function updateDebugInfo() {
-        var debugPanel = document.getElementById("debug");
-        debugPanel.innerHTML = [
+        _debug.innerHTML = [
             "<table>",
             "<tr><td>Size and offset:</td><td>", _width, "x", _height,
             "@", _scrollY, "</td></tr>",
             "</table>"
         ].join("");
-        debugPanel.style.top = _scrollY + "px";
-        debugPanel.style.left = Math.max(_width - 320, 4) + "px";
     }
 
-    function _onScroll(event) {
-        event = event || window.event;
-        _scrollY = window.scrollY;
-        updateDebugInfo();
-    }
-
-    function _onResize(event) {
-        event = event || window.event;
-        _width = window.innerWidth;
-        _height = window.innerHeight;
-        document.body.style.height = _height + "px";
-        updateDebugInfo();
-    }
+    gpf.html.responsive()
+        .addEventListener("scroll", function (event) {
+            _scrollY = event.get("top");
+            _debug.style.top = _scrollY + "px";
+            updateDebugInfo();
+        })
+        .addEventListener("resize", function (event) {
+            _width = event.get("width");
+            _height = event.get("height");
+            document.body.style.height = _height + "px";
+            _debug.style.left = Math.max(_width - 320, 4) + "px";
+            updateDebugInfo();
+        });
 
     // Install handlers
-    window.onscroll = _onScroll;
-    window.onresize = _onResize;
     window.email = function () {
         alert("email!");
     };
-
-    _onResize();
-    _onScroll();
 }
 
-window.onload=function () {
+window.onload = function () {
     "use strict";
     gpf.loaded(main);
 };
